576d974f09cc7e740a6a46f3b98f75a62b8f1621
adv <jxradv@gmail.com>
2025-02-04 18:30:50 +0500

    qwer

--- attestapp/admin.py
+++ attestapp/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import AttastModel
 # Register your models here.
+class AdminPanel(admin.ModelAdmin):
+    list_display = ['ism', 'tell', 'ruyxatdanUtishVaqti'] 
+    
+
+admin.site.register(AttastModel, AdminPanel)
